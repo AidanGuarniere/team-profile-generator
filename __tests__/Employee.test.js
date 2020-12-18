98f2c049e807555f74e8fa7e54d52883d3a029ee
aidan <aidanguarniere@gmail.com>
2020-12-18 12:23:42 -0800

    created, failed, then passed test to get general Employee info

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,10 +1,17 @@
-const Employee = require('../lib/employee')
+const Employee = require("../lib/employee");
 
-test('creates an employee object', () => {
-    const employee = new Employee('Dave', '1', 'dave@davemail.com', 'Intern');
-    expect(employee.name).toBe('Dave');
-    expect(employee.id).toBe('1');
-    expect(employee.email).toBe('dave@davemail.com');
-    expect(employee.position).toBe('Intern')
+test("creates an employee object", () => {
+  const employee = new Employee("Dave", "1", "dave@davemail.com", "Intern");
+  expect(employee.name).toBe("Dave");
+  expect(employee.id).toBe("1");
+  expect(employee.email).toBe("dave@davemail.com");
+  expect(employee.position).toBe("Intern");
+});
 
+test("returns general employee info", () => {
+  const employee = new Employee("Dave", "1", "dave@davemail.com", "");
+
+  expect(employee.getGeneralInfo()).toHaveProperty("name");
+  expect(employee.getGeneralInfo()).toHaveProperty("id");
+  expect(employee.getGeneralInfo()).toHaveProperty("email");
 });
