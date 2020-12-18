4c48b44badc7e0528b473fd330632366d1a514eb
aidan <aidanguarniere@gmail.com>
2020-12-18 15:59:03 -0800

    forgot to make a new feature branch

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,25 @@
+const Intern = require("../lib/intern");
+
+
+test("creates an Intern object", () => {
+    const intern = new Intern("Dave", "1", "dave@davemail.com", "Intern", "dave university");
+    expect(intern.position).toBe("Intern");
+    expect(intern.school).toBe("dave university")
+  });
+
+//   test("returns Intern Github", () => {
+//     const intern = new Intern("Dave", "1", "dave@davemail.com", "Intern", "dave university");
+  
+//     expect(intern.getGithub()).toHaveProperty("github");
+//   });
+
+//   test("returns Intern position", () => {
+//     const intern = new Intern("", "", "", "Intern", "dave university");
+  
+//     expect(intern.getPosition()).toHaveProperty("position");
+
+//     expect(intern.position).toBe("Intern")
+
+//     console.log(intern);
+
+//   });
